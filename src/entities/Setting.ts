0e46cd1c3c0359d11bb5b2b995acc4473df29086
joao96 <jvpoletti@gmail.com>
2021-12-13 15:53:56 -0300

    ✨ adding settings table with repo and controller

---
+++ src/entities/Setting.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('settings')
+class Setting {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  chat: boolean;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Setting };
