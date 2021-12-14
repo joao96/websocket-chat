e44b3064feddd3608ad1de45e477b9f97f997684
joao96 <jvpoletti@gmail.com>
2021-12-14 15:15:27 -0300

    ✨ adding users repo + service + controller

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,20 +1,19 @@
 import { Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from '../repositories/SettingsRepository';
+import { SettingsService } from '../services/SettingsService';
 
 class SettingsController {
   async create(request: Request, response: Response): Promise<Response> {
     const { username, chat } = request.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
 
-    const settings = settingsRepository.create({
-      username,
-      chat,
-    });
+    const settingsService = new SettingsService();
 
-    await settingsRepository.save(settings);
+    try {
+      const settings = await settingsService.create({ chat, username });
 
-    return response.status(201).json(settings);
+      return response.status(201).json(settings);
+    } catch (err) {
+      return response.status(400).json({ message: err.message });
+    }
   }
 }
 
